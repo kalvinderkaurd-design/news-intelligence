f34f14f416a3049fe29760f9a3af51dee31257ed
Paramjeet Dhanjal <paramjeetkaurdhanjal8@gmail.com>
2026-05-14 17:21:02 +0530

    chore: update application port configuration to support environment variables and production hosting

--- main.py
+++ main.py
@@ -8,5 +8,6 @@ if __name__ == '__main__':
     db_dir = os.path.join(os.path.dirname(__file__), 'database')
     if not os.path.exists(db_dir):
         os.makedirs(db_dir)
-        
-    app.run(debug=True, port=5001)
+    
+    port = int(os.environ.get('PORT', 8080))  # Add this line
+    app.run(debug=False, host='0.0.0.0', port=port)  # Change this line
